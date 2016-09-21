9e4a5c066dc34a57faf9166ce3bc0e26265b80f6
hannako <jessjones1@gmail.com>
2016-09-21 16:39:36 +0100

    refactoring

--- lib/account.rb
+++ lib/account.rb
@@ -24,9 +24,4 @@ class Account
     transactions.print_statement
   end
 
-
-
-
-
-
 end
--- lib/transactions.rb
+++ lib/transactions.rb
@@ -6,33 +6,22 @@ attr_reader :statement
     @statement = []
   end
 
-
   def record_transaction(amount,balance)
-    transaction = {date:'',credit:'',debit:'',balance:''}
+    transaction = {date:'',credit:' 0 ',debit:' 0 ',balance:''}
     transaction['date'] = Time.now.strftime("%d/%m/%y")
     transaction['balance'] = balance
-    if amount >= 0
-      transaction['credit']=amount
-      transaction['debit']= ''
-    else
-      transaction['debit']=amount.abs
-      transaction['credit']= ''
-    end
+    amount > 0 ? transaction['credit']=amount : transaction['debit']= amount.abs
     @statement << transaction
   end
 
   def print_statement
-    print "||  date  ||credit||debit||balance||"
-    print "\n"
+    print "||  date  ||credit||debit||balance|| \n"
     @statement.each do |transaction|
       print "   #{transaction['date']}"
       print "   #{transaction['credit']}"
       print "       #{transaction['debit']}"
-      print "       #{transaction['balance']}"
-      print "\n"
+      print "       #{transaction['balance']} \n"
     end
   end
-
-
 
 end
